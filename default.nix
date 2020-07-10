6a6018ad65e898c3e8e29bc65377d646d670d852
Nick Bathum <nickbathum@gmail.com>
2020-07-10 06:14:40 -0400

    Basic 2020 updates
    Stub in information for 2020: s/2019/2020
    Remove 2019 copied info, i.e. location
    
    Update nixpkgs src to nixos-20.03, hopefully this is correct?
    Using gitignoreSrc to ignore "result"
    
    Point logo image at reduced-filesize logo svg. (same as 2019 site)

--- default.nix
+++ default.nix
@@ -1,9 +1,19 @@
 { pkgs ? import ./nixpkgs {} }:
 
+let
+  gitignoreSrc = pkgs.fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "gitignore";
+    rev = "647d0821b590ee96056f4593640534542d8700e5";
+    sha256 = "sha256:0ks37vclz2jww9q0fvkk9jyhscw0ial8yx2fpakra994dm12yy1d";
+  };
+  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
+in
+
 pkgs.stdenv.mkDerivation {
   name = "nixcon-website";
 
-  src = ./.;
+  src = gitignoreSource ./.;
 
   buildInputs = with pkgs; [
     glibcLocales
