174bbe896e353f22a995ef9f6b85773fd0cf1026
thilo <thilo.billerbeck@officerent.de>
2020-08-16 16:17:55 +0200

    added mobile menu and mobile cta button

--- js/index.js
+++ js/index.js
@@ -1,19 +1,28 @@
-function emailReplace (elems){
-    var m = "orgateam" + String.fromCharCode(64) + "nixcon.org";
-    elems.getAttribute("href", "mailto:" + m)
-    elems.textContent = m
+function emailReplace(elems) {
+  var m = "orgateam" + String.fromCharCode(64) + "nixcon.org";
+  elems.getAttribute("href", "mailto:" + m)
+  elems.textContent = m
 }
 
 function ready(fn) {
-    if (document.readyState != 'loading'){
-      fn();
-    } else {
-      document.addEventListener('DOMContentLoaded', fn);
-    }
+  if (document.readyState != 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function toggleNav() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
 function initSite() {
-    document.querySelectorAll("a.contact-email").forEach(e => emailReplace(e));
+  document.querySelectorAll("a.contact-email").forEach(e => emailReplace(e));
 }
 
 ready(initSite);
